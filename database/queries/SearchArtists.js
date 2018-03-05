d0f3f0c966418a4537f6e7bbaf53c6291a155eaa
bnoden <brandroid369@gmail.com>
2018-03-05 14:27:09 -0600

    .

--- database/queries/SearchArtists.js
+++ database/queries/SearchArtists.js
@@ -2,20 +2,23 @@ const Artist = require('../models/artist');
 
 /**
  * Searches through the Artist collection
- * @param {object} criteria An object with a name, age, and yearsActive
+ * @param {object} arg An object with a name, age, and yearsActive
  * @param {string} sortProperty The property to sort the results by
  * @param {integer} offset How many records to skip in the result set
  * @param {integer} limit How many records to return in the result set
  * @return {promise} A promise that resolves with the artists, count, offset, and limit
  * like this: { all: [artists], count: count, offset: offset, limit: limit }
  */
-module.exports = (criteria, sortProperty, offset = 0, limit = 10) => {
-  const query = Artist.find(buildQuery(criteria))
+module.exports = (arg, sortProperty, offset = 0, limit = 10) => {
+  const fullQuery = arg => Artist.find(buildQuery(arg));
+  const resultCount = arg => fullQuery(arg).count();
+
+  const query = fullQuery(arg)
     .sort({ [sortProperty]: 1 }) // [sortProperty] is ES6 interpolated object key, not array
     .skip(offset)
     .limit(limit);
 
-  return Promise.all([query, Artist.count()]).then(results => ({
+  return Promise.all([query, resultCount(arg)]).then(results => ({
     all: results[0],
     count: results[1],
     offset,
@@ -23,9 +26,9 @@ module.exports = (criteria, sortProperty, offset = 0, limit = 10) => {
   }));
 };
 
-const buildQuery = criteria => {
+const buildQuery = arg => {
   const query = {};
-  const { age, name, yearsActive } = criteria;
+  const { age, name, yearsActive } = arg;
 
   if (name) {
     query.$text = { $search: name };
@@ -35,7 +38,6 @@ const buildQuery = criteria => {
       $gte: age.min,
       $lte: age.max
     };
-    console.log(query.age);
   }
   if (yearsActive) {
     query.yearsActive = {
